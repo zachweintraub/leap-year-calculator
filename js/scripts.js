208db5af015bcfee568543773be109a84707b9d1
Maya Frame and Zach Weintraub <zach@>
2019-04-02 15:51:34 -0700

    add refractoring

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,6 @@
 
 var leapYear = function(year) {
-  if (year % 100 === 0) {
-    return false;
-  } else if (year % 4 === 0) {
+  if ((year % 4 === 0) && (year % 100 !== 0)) {
     return true;
   } else {
     return false;
