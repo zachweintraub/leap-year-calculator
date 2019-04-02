b1ab4c68a92b07cf5f644e13946f19196d36fca7
Maya Frame and Zach Weintraub <zach@>
2019-04-02 15:45:13 -0700

    initial commit, HTML form structure, first BBD specs

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+
+var leapYear = function(year) {
+  if (year % 100 === 0) {
+    return false;
+  } else if (year % 4 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+
+
+$(document).ready(function() {
+  $("form#leap-year").submit(function(event) {
+    event.preventDefault();
+    var year = parseInt($("input#year").val());
+    var result = leapYear(year);
+    $("#result").text(result);
+  });
+});
