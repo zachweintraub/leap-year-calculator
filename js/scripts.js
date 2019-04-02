fc4ba3b839aefb1b9bb79b60708f97207a6f0242
Maya Frame and Zach Weintraub <zach@>
2019-04-02 16:44:55 -0700

    add front end logic

--- js/scripts.js
+++ js/scripts.js
@@ -14,6 +14,14 @@ $(document).ready(function() {
     event.preventDefault();
     var year = parseInt($("input#year").val());
     var result = leapYear(year);
-    $("#result").text(result);
-  });
-});
+    $(".year").text(year);
+
+     if (!result) {                 // same as writing if (result === false)
+       $(".not").text("not");
+     } else {
+       $(".not").text("");
+     }
+
+     $("#result").show();
+   });
+ });
